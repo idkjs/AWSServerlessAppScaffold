3a326b2df276f231aca812da652f1b8f16f7ba9c
Gustavo Tavares <grstavares@gmail.com>
2018-09-16 18:17:27 -0300

    [UPDATE] CRUD working

--- empresa/main.ts
+++ empresa/main.ts
@@ -8,13 +8,6 @@ import * as uuid from 'uuid';
 const dynamoDb = new DynamoDB.DocumentClient();
 const tableName = process.env.DynamoTable;
 
-function uploadItem(item: Object): Promise<DynamoDB.DocumentClient.PutItemOutput> {
-
-  const params:DynamoDB.DocumentClient.PutItemInput = { TableName: tableName, Item: item};
-  return dynamoDb.put(params).promise();
-
-}
-
 function getItem(id: string): Promise<DynamoDB.DocumentClient.GetItemOutput> {
 
   const params: DynamoDB.DocumentClient.GetItemInput = { TableName: tableName, Key: {id: id}};
@@ -22,11 +15,51 @@ function getItem(id: string): Promise<DynamoDB.DocumentClient.GetItemOutput> {
 
 }
 
+function uploadItem(item: Object): Promise<DynamoDB.DocumentClient.PutItemOutput> {
+
+  const params:DynamoDB.DocumentClient.PutItemInput = { TableName: tableName, Item: item};
+  return dynamoDb.put(params).promise();
+
+}
+
+function deleteItem(id: string): Promise<DynamoDB.DocumentClient.DeleteItemOutput> {
+
+  const params: DynamoDB.DocumentClient.DeleteItemInput = { TableName: tableName, Key: {id: id}};
+  return dynamoDb.delete(params).promise();
+
+}
+
+function hasValidObjectKey(object: Object, id: string): Boolean {
+
+  if (!object.hasOwnProperty('id')) {return false;}
+  if (id !== object['id']) {return false;}
+  return true;
+
+}
+
 export const handler: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
   
   const method = event.httpMethod;
+  const id = event.path.substr(1);
 
   switch (method) {
+
+    case 'GET': {
+
+      getItem(id).then(data => {
+
+        const response = { statusCode: 200, body: JSON.stringify(data.Item)};
+        cb(null, response);
+
+      }).catch(err => {
+
+        cb(err, null);
+
+      });
+
+      break;
+
+    }
 
     case 'POST': {
 
@@ -53,12 +86,38 @@ export const handler: Handler = (event: APIGatewayEvent, context: Context, cb: C
 
     }
 
-    case 'GET': {
+    case 'PUT': {
 
-      const id = event.path.substr(1);
-      getItem(id).then(data => {
+      const updated = JSON.parse(event.body);
+      if (hasValidObjectKey(updated, id)) {
 
-        const response = { statusCode: 200, body: JSON.stringify(data.Item)};
+        uploadItem(updated).then(data => {
+
+          const response = { statusCode: 200, body: null};
+          cb(null, response);
+  
+        }).catch(err => {
+  
+          cb(err, null);
+  
+        });
+
+      } else {
+
+        const response = { statusCode: 405, message: 'Invalid Object Body! Invalid Id!'};
+        cb(null, response)
+
+      }
+      
+      break;
+
+    }
+
+    case 'DELETE': {
+
+      deleteItem(id).then(data => {
+
+        const response = { statusCode: 200, body: null};
         cb(null, response);
 
       }).catch(err => {
@@ -71,23 +130,6 @@ export const handler: Handler = (event: APIGatewayEvent, context: Context, cb: C
 
     }
 
-    case 'PUT': {
-
-      const returned = {id: '12345', name: 'Testtando'}
-      const response = { statusCode: 200, body: JSON.stringify(returned)};
-      cb(null, response);
-      break;
-
-    }
-
-    case 'DELETE': {
-
-      const returned = {id: '12345', name: 'Testtando'}
-      const response = { statusCode: 200, body: JSON.stringify(returned)};
-      cb(null, response);
-      break;
-
-    }
     case 'PATCH': {
 
       const returned = {id: '12345', name: 'Testtando'}
